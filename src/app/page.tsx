3335483790c754b636594c6c709ab493c9aef4b9
MeetParabiya <meet.parabiya@azilen.com>
2025-06-09 11:41:19 +0530

    Add new components and update Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import InfiniteCards from "@/components/InfiniteCards";
+import Instructors from "@/components/Instructors";
+import UpcoingWebinars from "@/components/UpcoingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -10,6 +13,9 @@ export default function Home() {
       <FeaturedCourses />
       <WhyChooseUs />
       <InfiniteCards />
+      <UpcoingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
--- src/components/InfiniteCards.tsx
+++ src/components/InfiniteCards.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+import { cn } from "@/lib/utils";
 
 const musicSchoolTestimonials = [
   {
@@ -36,7 +37,16 @@ const musicSchoolTestimonials = [
 
 function InfiniteCards() {
   return (
-    <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+    <div className="relative flex h-[40rem] w-full items-center justify-center bg-white dark:bg-black flex flex-col overflow-hidden">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:30px_30px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
       <h2 className="text-3xl font-bold text-center mb-8 z-10">
         Hear our Harmony: Voices of success
       </h2>
@@ -44,7 +54,7 @@ function InfiniteCards() {
         <div className="w-full max-w-6xl">
           <InfiniteMovingCards
             items={musicSchoolTestimonials}
-            direction="right"
+            direction="left"
             speed="slow"
           />
         </div>
